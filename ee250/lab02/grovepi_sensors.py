b57312c842f62b5f77d36eb265eaee4f81e39988
Rob Sutherland <rfsuther@usc.edu>
2020-08-25 19:50:28 -0400

    Update grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,9 +1,16 @@
-
 """ EE 250L Lab 02: GrovePi Sensors
 List team members here.
 Insert Github repository link here.
 """
 
+"""python3 interpreters in Ubuntu (and other linux distros) will look in a 
+default set of directories for modules when a program tries to `import` one. 
+Examples of some default directories are (but not limited to):
+  /usr/lib/python3.5
+  /usr/local/lib/python3.5/dist-packages
+The `sys` module, however, is a builtin that is written in and compiled in C for
+performance. Because of this, you will not find this in the default directories.
+"""
 import sys
 import time
 # By appending the folder of all the GrovePi libraries to the system path here,
@@ -30,37 +37,32 @@ full_distance = 517
 
 
 ##MAIN
-    while True:
-        #So we do not poll the sensors too quickly which may introduce noise,
-        #sleep for a reasonable time of 200ms between each iteration.
-        time.sleep(0.2)
+while True:
+    #So we do not poll the sensors too quickly which may introduce noise,
+    #sleep for a reasonable time of 200ms between each iteration.
+    time.sleep(0.2)
 
-        #ultIN = grovepi.ultrasonicRead(PORT)
-        potIN = grovepi.analogRead(potPrt)
-
-
-        print(potIN)
-        # if oldThresh != newThresh
+    #ultIN = grovepi.ultrasonicRead(PORT)
+    potIN = grovepi.analogRead(potPrt)
 
 
-        # if ...:                            #   object
-        # 	setRGB(255,10,20) #Sets red (R,G,B)
-        # 	setText_norefresh("Set text here A {}...".format(str(X)) )
+    print(potIN)
+    # if oldThresh != newThresh
 
 
-
-        # else...:                           # no object
-        # 	setRGB(10,255,30) #SET TO GREEN
-
-        # 	setText_norefresh("Set text here B {}...".format(str(Y)) )
-
-
-       	# (.....BUFFFER CLEAR)
+    # if ...:                            #   object
+    # 	setRGB(255,10,20) #Sets red (R,G,B)
+    # 	setText_norefresh("Set text here A {}...".format(str(X)) )
 
 
 
+    # else...:                           # no object
+    # 	setRGB(10,255,30) #SET TO GREEN
+
+    # 	setText_norefresh("Set text here B {}...".format(str(Y)) )
 
 
+   	# (.....BUFFFER CLEAR)
 
 
 
