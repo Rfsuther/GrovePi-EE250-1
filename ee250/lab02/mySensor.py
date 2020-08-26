20fd6a7cb1faf1b9d5d3d5f72c89b25f4d82dfd4
Rob Sutherland <rfsuther@usc.edu>
2020-08-25 20:42:25 -0400

    Update mySensor.py

--- ee250/lab02/mySensor.py
+++ ee250/lab02/mySensor.py
@@ -58,19 +58,19 @@ while True:
 
 
 
-	if count < 100
+	if count < 100:
 
 		setText("Hello world\nLCD test")
 		setRGB(0,128,64)
 		count += 1
 		time.sleep(0.1)
 
-	else if 100<count and count <200
+	else if 100<count and count <200:
 
 		setText("Goodbyeeee\nLCD test")
 		setRGB(128,0,64)
 		count += 1
-	else
+	else:
 		count=0
 		time.sleep(0.1)
 
