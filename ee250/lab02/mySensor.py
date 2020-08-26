e2fc9096f1a7ec3ebfe5d4388ab3643b44d341f5
Rob Sutherland <rfsuther@usc.edu>
2020-08-26 12:56:22 -0400

    Update mySensor.py

--- ee250/lab02/mySensor.py
+++ ee250/lab02/mySensor.py
@@ -90,7 +90,7 @@ while True:
 
 	setText_norefresh(f"\n {dist2obj}cm")
 
-	if dist2obj < newThresh                   #   object
+	if dist2obj < newThresh:                   #   object
 		setRGB(255,10,20) #Sets red (R,G,B)
 		setText_norefresh("1234{newThresh}")
 
@@ -101,5 +101,3 @@ while True:
 
 		setText_norefresh(f"23456{newThresh}")
 
-
-		(.....BUFFFER CLEAR)
